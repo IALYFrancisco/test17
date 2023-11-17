b63ddef2586d376a5cc33bcc8527724524258641
IALY <franciscoraymondialy@gmail.com>
2023-11-17 11:47:38 +0300

    ajout de mis à jour

---
+++ script/script.js
@@ -0,0 +1,7 @@
+var form = document.querySelector('form');
+
+window.addEventListener('load', () => {
+    form.style.opacity='1';
+    form.style.transition='1s';
+    form.style.marginTop='50px';
+});
